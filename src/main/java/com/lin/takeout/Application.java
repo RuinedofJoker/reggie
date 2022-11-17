0f4ee127275c00e1908ca571c5b3607ee32dc273
joker <616401600@qq.com>
2022-11-17 09:21:15 +0800

    11/17 9:15 项目完成

--- src/main/java/com/lin/takeout/Application.java
+++ src/main/java/com/lin/takeout/Application.java
@@ -10,10 +10,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
-public class ReggieApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReggieApplication.class, args);
+        SpringApplication.run(Application.class, args);
         log.info("项目启动成功...");
     }
 
